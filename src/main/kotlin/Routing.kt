faf2f132dd4e50aa890f73e4e2f1032ff3fcc66c
Abakar Magomedov <AMagomedov2@alfabank.ru>
2025-01-29 18:28:54 +0300

    Initial Commit

---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,24 @@
+package com.abocha
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+
+        get("/books") {
+            call.respondText("I will return here list of books")
+        }
+
+        get("/bookPart") {
+            call.respondText("I will return here part of book")
+        }
+    }
+}
+
+
+
