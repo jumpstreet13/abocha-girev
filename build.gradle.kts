6d5b29a74f13bb61d09dc8942eeac2befc919f4c
Abakar Magomedov <AMagomedov2@alfabank.ru>
2025-01-30 21:15:19 +0300

    add docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ version = "0.0.1"
 application {
     mainClass.set("com.abocha.KtorServerKt")
 
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+    /*val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")*/
 }
 
 repositories {
